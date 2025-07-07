8373d94182dc40eaaa0999f5caf02f72c97d1c85
bruno-reis-castro <bruno.reis.castro1@gmail.com>
2025-07-07 20:20:45 -0300

    adicionando fixtures para gerir corpos das req

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,8 @@ const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencia = require ('../fixtures/postTransferencia.json')
+
 
 describe('Trasnferencias', () => {
     describe('POST/transferencias', () => {
@@ -13,33 +15,25 @@ describe('Trasnferencias', () => {
 
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10 reais', async () =>{
             //captura token
-
+            
+            const bodyTransferencia = { ...postTransferencia }
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
+                .send(bodyTransferencia)
             expect(resposta.status).to.equal(201)
             console.log(resposta.body)            
         })
 
         it('Deve retornar sucesso com 422 quando o valor da transferencia for abaixo de 10 reais', async () =>{           
-
+            const bodyTransferencia = { ...postTransferencia }
+            bodyTransferencia.valor = 1
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 1,
-                    token: ""
-                })
+                .send(bodyTransferencia)
             expect(resposta.status).to.equal(422)
             console.log(resposta.body)           
         })
